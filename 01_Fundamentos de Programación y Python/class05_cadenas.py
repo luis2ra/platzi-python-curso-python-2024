cb5a6dd2eff5d4fce37f8a302e3af26a6adbbe10
Luis Altuve <luis2ra@gmail.com>
2025-06-11 16:28:35 -0500

    updated module of course

---
+++ 01_Fundamentos de Programación y Python/class05_cadenas.py
@@ -0,0 +1,19 @@
+nombre1 = "Luis con comillas dobles"
+nombre2 = "Luis con comillas simples"
+nombre3 = """luis 
+con comillas 
+triples
+"""
+print(nombre1)
+print(nombre2)
+print(nombre3)
+
+name = "CARLA Marcela"
+last_name = "   Florida     Roman  "
+print(5 * name)
+print(name + " " + last_name)
+print(len(name))
+print(len(last_name))
+print(name.lower())
+print(name.upper())
+print(last_name.strip())
