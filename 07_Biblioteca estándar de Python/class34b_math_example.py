eeb255ff9a2aa91c3f2956e56a50f0bb35c1e612
Luis Altuve <luis2ra@gmail.com>
2025-07-11 16:10:04 -0500

    updated module 07 of course

--- 07_Biblioteca estándar de Python/class34b_math_example.py
+++ 07_Biblioteca estándar de Python/class34b_math_example.py
@@ -1,7 +1,8 @@
 import math
 
-#Hallar el area y perimetro de un circulo
-radius = 5
+
+# Hallar el area y perimetro de un circulo
+radius = 7
 area = math.pi * radius**2
 perimeter = 2 * math.pi * radius
 print(area)
--- 07_Biblioteca estándar de Python/class35_statistics_example.py
+++ 07_Biblioteca estándar de Python/class35_statistics_example.py
@@ -3,33 +3,34 @@ import csv
 
 # Leer los datos de ventas mensuales desde un archivo CSV
 monthly_sales = {}
-with open('monthly_sales.csv', mode='r') as file:
+with open("class35_monthly_sales.csv", mode="r") as file:
     reader = csv.DictReader(file)
     for row in reader:
-        month = row['month']
-        sales = int(row['sales'])
+        month = row["Month"]
+        sales = int(row["Sales"])
         monthly_sales[month] = sales
 
 sales = list(monthly_sales.values())
-#print(sales)
+print(sales)
+print()
 
-#Hallar la media
+# Hallar la media
 mean_sales = statistics.mean(sales)
 print(f"La media es: {mean_sales}")
 
-#Hallar la mediana
+# Hallar la mediana
 median_sales = statistics.median(sales)
 print(f"La mediana es: {median_sales}")
 
-#Hallar la moda
+# Hallar la moda
 mode_sales = statistics.mode(sales)
 print(f"La moda es: {mode_sales}")
 
-#Desviación Estándar
+# Desviación Estándar
 stdev_sales = statistics.stdev(sales)
 print(f"La desviación estándar es: {stdev_sales}")
 
-#Hallar la varianza
+# Hallar la varianza
 variance_sales = statistics.variance(sales)
 print(f"La moda es: {variance_sales}")
 
@@ -37,4 +38,4 @@ max_sales = max(sales)
 min_sales = min(sales)
 
 range_sales = max_sales - min_sales
-print(f'Rango de ventas: {range_sales}')
+print(f"Rango de ventas: {range_sales}")
